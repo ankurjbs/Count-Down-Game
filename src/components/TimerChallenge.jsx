72d963a00dd960406446631c49996d902daf5a33
Ankur Maurya <31955983+ankurjbs@users.noreply.github.com>
2024-05-26 21:03:58 +0530

    Mananging timer variable using ref hooks

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
+
 
 export default function TimerChallenge({ title, targetTime }) {
+  // this timer is defined inside the component ,so it is component specific. So it will work totally independent for each instances of components
+  const timer = useRef(); 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timeExpired, setTimeExpired] = useState(false);
 
+  
+
   function handleStart() {
     setTimerStarted(true);
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimeExpired(true);
     }, targetTime * 1000);
   }
 
   // to stop the challenge we have to create  one more function to handleStop but in this we have to use setTimeout from handleStart. so we will use ref hooks to tackle this issue.
-
+  // suppose we set the variable timer= setTimeOut in handleStart and use it in clearTimeOut(timer). If we clicking two button simultaneously React will throw away first clicked timer variable and you will loss the game. So solution the ref
+function handleStop(){
+  clearTimeout(timer.current);
+}
   return (
     <section className="challenge">
       <h2>{title}</h2>
@@ -21,7 +29,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
