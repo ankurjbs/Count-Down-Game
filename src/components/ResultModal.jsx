62a03f3f6c00e57e750d5291b08c3590fd0cddc8
ankurjbs <ankurjbs@gmail.com>
2024-05-28 15:13:13 +0530

    Won case implemetend

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -4,6 +4,9 @@ const ResultModal =  forwardRef(function ResultModal({onReset,targetTime,remaini
     const dialog = useRef();
     const userLost = remainingTime <=0;
     const formatRemainingTime = (remainingTime/1000).toFixed(2);
+    const score = Math.round((1- remainingTime/(targetTime*1000)) *100);
+
+
     // to use following hook we have to use use beacuse we now need seperatre ref to detach dialof ref from other outer component(TimerChallenge)
     useImperativeHandle(ref,()=>{
         return{
@@ -17,9 +20,10 @@ const ResultModal =  forwardRef(function ResultModal({onReset,targetTime,remaini
     // return <dialog ref={ref} className="result-modal">
     return <dialog ref={dialog} className="result-modal">
        {userLost && <h2>User Lost</h2>} 
+       {!userLost && <h2>User Won: Your score {score}</h2>}
         <p>The Target Timer was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>{formatRemainingTime} seconds left.</strong></p>
-        <form method="dialog" onSubmit={onResets}>
+        <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
     </dialog>;
