f7a2b112b4a4da0f16d9ca48900adefeb58cfa13
ankurjbs <ankurjbs@gmail.com>
2024-05-28 14:34:35 +0530

    set expire time

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,9 @@
 import { forwardRef, useImperativeHandle ,useRef} from "react";
 // we can't use ref props directly to the function. if we want to pass one ref to another we used to special type of hooks forwardRef
-const ResultModal =  forwardRef(function ResultModal({result,targetTime},ref){
+const ResultModal =  forwardRef(function ResultModal({result,targetTime,remainingTime},ref){
     const dialog = useRef();
-
+    const userLost = remainingTime <=0;
+    const formatRemainingTime = (remainingTime/1000).toFixed(2);
     // to use following hook we have to use use beacuse we now need seperatre ref to detach dialof ref from other outer component(TimerChallenge)
     useImperativeHandle(ref,()=>{
         return{
@@ -15,9 +16,9 @@ const ResultModal =  forwardRef(function ResultModal({result,targetTime},ref){
 
     // return <dialog ref={ref} className="result-modal">
     return <dialog ref={dialog} className="result-modal">
-        <h2>You {result}</h2>
+       {userLost && <h2>User Lost</h2>} 
         <p>The Target Timer was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the timer with <strong>X seconds left.</strong></p>
+        <p>You stopped the timer with <strong>{formatRemainingTime} seconds left.</strong></p>
         <form method="dialog">
             <button>Close</button>
         </form>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -6,42 +6,60 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timeExpired, setTimeExpired] = useState(false);
+  // const [timerStarted, setTimerStarted] = useState(false);
+  // const [timeExpired, setTimeExpired] = useState(false);
+  // to calcluate remainig time and mange state we wil use following state not above
+  const [timeRemaining,setTimeRemaining] = useState(targetTime*1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if(timeRemaining <= 0){
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
+
+  function handleResetTimer(){
+    setTimeRemaining(targetTime * 1000);
+  }
 
   function handleStart() {
-    
-    setTimerStarted(true);
-    timer.current = setTimeout(() => {
+    /* timer.current = setTimeout(() => {
+       setTimerStarted(true);
       // this showModel is built in js function of dialog element. Suppose more than one developer is working on it. and he changed to <dialog> to /<div> this will not work because this is the property of dialog element to show the modal. Show to solve this issue we use a custom funtion 'open' and intililze through callable function 'useImperativeHandle' hook in the result component
       // dialog.current.showModal();
       dialog.current.open(); // 'open' name is up to you
-    }, targetTime * 1000);
+    }, targetTime * 1000); */
+
+    // since we need remaining time so we will use following instead of the above.
+    timer.current = setInterval(()=>{
+      setTimeRemaining(prevTimeReamining => prevTimeReamining - 10);
+    },10);
   }
 
   // to stop the challenge we have to create  one more function to handleStop but in this we have to use setTimeout from handleStart. so we will use ref hooks to tackle this issue.
   // suppose we set the variable timer= setTimeOut in handleStart and use it in clearTimeOut(timer). If we clicking two button simultaneously React will throw away first clicked timer variable and you will loss the game. So solution the ref
   function handleStop() {
-    clearTimeout(timer.current);
-    setTimerStarted(false);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
   return (
     <>
     {/* we can not send ref directlly to the component */}
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
+      <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} result={timerIsActive?'won':'loss'}/>
       <section className="challenge">
         <h2>{title}</h2>
-        {timeExpired && <p>You Lost</p>}
+        {timerIsActive && <p>You Lost</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer is In Active"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer is In Active"}
         </p>
       </section>
     </>
