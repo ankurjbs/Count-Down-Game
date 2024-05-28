600c449aac17f73e15ed9c2f79d4475923e44f3e
ankurjbs <ankurjbs@gmail.com>
2024-05-28 15:30:42 +0530

    Portal is implemented

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,9 @@
 import { forwardRef, useImperativeHandle ,useRef} from "react";
+import { createPortal } from "react-dom"; // for ineration with DOM. Purpose of this to render the ResultComponenet to another place in DOM
+
+// since modal(ResultModal) is nested somewhere in the dom because I am using this in timechallenge jss. But technicall this should be reside on top of the element . purpose to styling or for something else so we use portals
+
+
 // we can't use ref props directly to the function. if we want to pass one ref to another we used to special type of hooks forwardRef
 const ResultModal =  forwardRef(function ResultModal({onReset,targetTime,remainingTime},ref){
     const dialog = useRef();
@@ -18,7 +23,8 @@ const ResultModal =  forwardRef(function ResultModal({onReset,targetTime,remaini
 
 
     // return <dialog ref={ref} className="result-modal">
-    return <dialog ref={dialog} className="result-modal">
+    // use createPortal to Teleport accept to parameters . One is JSX, and 2nd where code should be rendered
+    return createPortal(<dialog ref={dialog} className="result-modal">
        {userLost && <h2>User Lost</h2>} 
        {!userLost && <h2>User Won: Your score {score}</h2>}
         <p>The Target Timer was <strong>{targetTime} seconds.</strong></p>
@@ -26,7 +32,8 @@ const ResultModal =  forwardRef(function ResultModal({onReset,targetTime,remaini
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>;
+    </dialog>,document.getElementById('modal'));
+    // this modal id is in index.html
 })
 
 export default ResultModal;
